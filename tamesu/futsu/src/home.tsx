e39f60403a8c0f7c2a80c954b1b7f9f653f5e6b7
kyohei <kyohei103@outlook.jp>
2025-08-26 07:14:32 +0900

    tamesu

---
+++ tamesu/futsu/src/home.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+	return (
+		<div className="antialiased p-5 w-full flex justify-center">
+      <div className="max-w-3xl w-full flex flex-col">
+  			<h1 className="text-[1.35rem]">実験場</h1>
+      </div>
+		</div>
+	)
+}
