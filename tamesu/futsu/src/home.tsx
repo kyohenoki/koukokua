81a9b41407e5b073da5d63f8596056d8efe306e5
kyohei <kyohei103@outlook.jp>
2025-08-26 07:47:27 +0900

    koukokua

--- tamesu/futsu/src/home.tsx
+++ tamesu/futsu/src/home.tsx
@@ -1,9 +1,13 @@
+import { tasu } from 'koukokua'
+
 export default function Home() {
+	const kotae = tasu(10, 20).toString()
 	return (
 		<div className="antialiased p-5 w-full flex justify-center">
-      <div className="max-w-3xl w-full flex flex-col">
-  			<h1 className="text-[1.35rem]">実験場</h1>
-      </div>
+			<div className="max-w-3xl w-full flex flex-col">
+				<h1 className="text-[1.35rem]">実験場</h1>
+				<h1 className="text-[1.35rem] text-stone-500">{kotae}</h1>
+			</div>
 		</div>
 	)
 }
